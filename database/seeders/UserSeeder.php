396fe7ad1d3e3bb7aeed44fbcf64dddcc510a798
tahungoding <tahungoding@gmail.com>
2021-08-21 23:37:32 +0700

    fix user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -16,11 +18,24 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        $user = User::create([
             'name' => 'TAHUNGODING',
             'username' => 'tahungoding',
             'email' => 'tahungoding@gmail.com',
             'password' => Hash::make('password'),
         ]);
+
+        $role = Role::create(['name' => 'admin']);
+        $user->assignRole([$role->id]);
+
+        $user2 = User::create([
+            'name' => 'INIMAHSUMEDANG',
+            'username' => 'inimahsumedang',
+            'email' => 'inimahsumedang@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $role2 = Role::create(['name' => 'guest']);
+        $user2->assignRole([$role2->id]);
     }
 }
